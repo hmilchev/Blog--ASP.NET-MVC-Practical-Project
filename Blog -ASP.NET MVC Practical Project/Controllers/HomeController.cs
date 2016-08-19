18e1dc2ebade1c901584d98e3aa998e60e9ca67c
Hristo Milchev <hmilchev@icloud.com>
2016-08-19 22:44:05 +0300

    few edits done

--- Blog -ASP.NET MVC Practical Project/Controllers/HomeController.cs
+++ Blog -ASP.NET MVC Practical Project/Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace Blog__ASP.NET_MVC_Practical_Project.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
