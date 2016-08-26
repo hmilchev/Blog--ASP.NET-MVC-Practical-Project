65a0fe90a54e7345aed2661734594dfd5df0919a
Hristo Milchev <hmilchev@icloud.com>
2016-08-26 19:57:06 +0300

    Creating Utils
    Creating Classes - Additional > utils.cs , in which to store some
    additional methods. Creating the CutText method , to reduse strings to
    desired lenght

---
+++ Blog -ASP.NET MVC Practical Project/Classes - Aditional/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog__ASP.NET_MVC_Practical_Project.Classes___Aditional
+{
+    public class Utils
+    {
+        public static string CutText(string text, int maxLenght = 100)
+        {
+            if (text == null||text.Length <= maxLenght)         
+            {
+                return text;
+            }
+            var shortText = text.Substring(0, maxLenght) + "...";
+            return shortText;
+        }
+    }
+}
